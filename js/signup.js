8c313bd13df50e05962cd83c550948703fa3d0af
Lehan Coetzee <coetzeel@live.co.za>
2016-10-18 18:04:37 +0200

    Fixed signup

--- js/signup.js
+++ js/signup.js
@@ -30,20 +30,19 @@ function validatePassword() {
 }
 
 function signUp() {
-    var name = $('#email').val();
-    var surname = $('#email').val();
+    var name = $('#name').val();
+    var surname = $('#surname').val();
     var email = $('#email').val();
-    var password = $('#email').val();
-    var conf_password = $('#email').val();
+    var password = $('#password').val();
     var payload = {name: name, surname: surname, email: email, password: password, admin: "false", system_id: "222"};
 
     jQuery.ajax( {
-        url: 'http://' + api_host + '/users',
+        url: 'http://' + api_host + '/users?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjU3MTEzZGFlNmVjZjgzOGRhOTg2ZmExNyIsIm5hbWUiOiJMZWhhbiIsInN1cm5hbWUiOiJDb2V0emVlIiwiZW1haWwiOiJjb2V0emVlbEBsaXZlLmNvLnphIiwiYWRtaW4iOnRydWUsInN5c3RlbV9pZCI6IiIsImlhdCI6MTQ3NjUzMzk1MSwiZXhwIjoxNDg2NTMzOTUwfQ.Eb6IMloV8xKCWoaUgL-JuDS09m7Trh4SE8v-v-YAJEI',
         type: 'POST',
         dataType: 'JSON',
         data: payload,
         success: function(response) {
-            if (response.success == false) {
+            if (response.success == false) {             
               $('#login-error').show();
               $('#login-error-msg').html(response.message);
             } else {               
